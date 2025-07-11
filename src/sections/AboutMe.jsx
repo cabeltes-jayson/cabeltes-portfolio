6586bfe663fc33abd88c8758cdb6eade4623394f
Jayson B. Cabeltes <cabeltes.jayson7@gmail.com>
2025-07-11 19:47:13 +0800

    Partial Prototype 2 80%

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./aboutme.css";
 
 // Import skill icons
@@ -11,48 +11,16 @@ import djangoIcon from "../assets/django.png";
 import iotIcon from "../assets/iot.png";
 import mobileIcon from "../assets/mobile.png";
 import phpIcon from "../assets/php.png";
-
-// Icon list
-const skillIcons = [reactIcon, jsIcon, xamppIcon, cloudIcon, pythonIcon, djangoIcon, iotIcon, mobileIcon, phpIcon];
+import cert1 from "../assets/cert1.png";
 
 const AboutMe = () => {
-  const marqueeRef = useRef(null);
-
-  useEffect(() => {
-    const marquee = marqueeRef.current;
-    let animationTimeout;
-
-    const stopAnimation = () => {
-      marquee.classList.add("paused");
-      clearTimeout(animationTimeout);
-    };
-
-    const startAnimation = () => {
-      clearTimeout(animationTimeout);
-      animationTimeout = setTimeout(() => {
-        marquee.classList.remove("paused");
-      }, 1500);
-    };
-
-    const handleScroll = () => {
-      stopAnimation();
-      startAnimation();
-    };
-
-    marquee.addEventListener("wheel", handleScroll);
-
-    return () => {
-      marquee.removeEventListener("wheel", handleScroll);
-    };
-  }, []);
-
   return (
     <section className="aboutme-section" id="aboutme">
       {/* Centered Section Heading */}
       <h2 className="aboutme-heading">About Me</h2>
 
       <div className="aboutme-container">
-        {/* Left - Bio */}
+        {/* TopLeft - Bio */}
         <div className="aboutme-left">
           <h3 className="section-title">Bio</h3>
           <p className="bio-description">
@@ -73,12 +41,12 @@ const AboutMe = () => {
           </div>
         </div>
 
-        {/* Right - Certificates */}
+        {/* TopRight - Certificates */}
         <div className="aboutme-right">
           <h3 className="section-title">Certificates</h3>
           <div className="certificates-wrapper">
             {/* 🖼️ Replace these divs with <img src="..." /> later */}
-            <div className="certificate-box">Certificate 1</div>
+            <img src={cert1} alt="cert1" />
             <div className="certificate-box">Certificate 2</div>
             <div className="certificate-box">Certificate 3</div>
             <div className="certificate-box">Certificate 4</div>
@@ -86,20 +54,82 @@ const AboutMe = () => {
         </div>
       </div>
 
-      {/* Bottom - Skills Bar */}
-      <div className="skills-section">
-        <h3 className="section-title center-text">Skills</h3>
-        {/* Marquee Container */}
-        <div className="skills-marquee-container" ref={marqueeRef}>
-          <div className="skills-marquee-track">
-            {[...skillIcons, ...skillIcons].map((icon, index) => (
-              <img
-                key={index}
-                src={icon}
-                alt={`skill-${index}`}
-                className="skill-icon"
-              />
-            ))}
+      {/* Bottom - Skills and Experiences Section */}
+      <div className="aboutme-lower-container">
+        {/* Left - Skills */}
+        <div className="aboutme-left">
+          <h3 className="section-title">Skills</h3>
+
+          <div className="scroller" data-speed="fast" data-animated="true" data-direction="left">
+            <ul className="tag-list inside-scroll">
+              <li>Python</li>
+              <li>Javascript</li>
+              <li>IoT</li>
+              <li>React</li>
+              <li>PHP</li>
+              <li>Django</li>
+              <li>Cloud</li>
+              <li>XAMPP</li>
+
+              <li>Python</li>
+              <li>Javascript</li>
+              <li>IoT</li>
+              <li>React</li>
+              <li>PHP</li>
+              <li>Django</li>
+              <li>Cloud</li>
+              <li>XAMPP</li>
+            </ul>
+          </div>
+
+          {/* ICONS */}
+          <div className="scroller" data-speed="fast" data-animated="true" data-direction="right">
+            <div className="tag-list inside-scroll">
+              <img src={pythonIcon} alt="Django" />
+              <img src={jsIcon} alt="Javascript" />
+              <img src={reactIcon} alt="React" />
+              <img src={xamppIcon} alt="XAMPP" />
+              <img src={cloudIcon} alt="Cloud" />
+              <img src={djangoIcon} alt="Django" />
+              <img src={iotIcon} alt="IoT" />
+              <img src={mobileIcon} alt="Mobile" />
+              <img src={phpIcon} alt="PHP" />
+
+              <img src={pythonIcon} alt="Django" />
+              <img src={jsIcon} alt="Javascript" />
+              <img src={reactIcon} alt="React" />
+              <img src={xamppIcon} alt="XAMPP" />
+              <img src={cloudIcon} alt="Cloud" />
+              <img src={djangoIcon} alt="Django" />
+              <img src={iotIcon} alt="IoT" />
+              <img src={mobileIcon} alt="Mobile" />
+              <img src={phpIcon} alt="PHP" />
+            </div>
+          
+          </div>
+
+        </div>
+
+        {/* Right - Experiences */}
+        <div className="aboutme-right">
+          <h3 className="section-title">Experiences</h3>
+          <div className="experiences-wrapper">
+            <div className="experience-box">
+              <h4>Bureau of Fire Protection</h4>
+              <p>Worked as IT support and developed a website.</p>
+            </div>
+            <div className="experience-box">
+              <h4>Experience Title 2</h4>
+              <p>Short description about this experience.</p>
+            </div>
+            <div className="experience-box">
+              <h4>Experience Title 3</h4>
+              <p>Short description about this experience.</p>
+            </div>
+            <div className="experience-box">
+              <h4>Experience Title 4</h4>
+              <p>Short description about this experience.</p>
+            </div>
           </div>
         </div>
       </div>
